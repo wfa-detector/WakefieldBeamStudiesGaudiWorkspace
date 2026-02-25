dda18c7971f30b01a5fde911300a1ad5848b1096
Angira Rastogi <arastogi@lbl.gov>
2026-02-24 16:55:45 -0800

    Adding extra libraries in the setup file

--- setup.sh
+++ setup.sh
@@ -25,6 +25,7 @@ export MYINSTALL=$(realpath ${MYINSTALL})
 # Set paths
 export PATH=${MYINSTALL}/bin:$PATH
 export LD_LIBRARY_PATH=${MYINSTALL}/lib:${MYINSTALL}/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/gsl-2.8-bc2rb22wiesshj5u3yod7rwo2sosqcis/lib:/opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/clhep-2.4.7.2-635b2rilemcklercdhk6redbewcpfre5/lib:/opt/spack/opt/spack/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/__spack_path_placeholder__/linux-x86_64/boost-1.89.0-sklrsfi2plrhbyjk5mrx56ev5zap3lir/lib:$LD_LIBRARY_PATH
 export ROOT_INCLUDE_PATH=${MYINSTALL}/include:$ROOT_INCLUDE_PATH
 export PYTHONPATH=${MYINSTALL}/python:$PYTHONPATH
 export CMAKE_PREFIX_PATH=${MYINSTALL}:$CMAKE_PREFIX_PATH
